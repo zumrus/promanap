6c6365ea243db1874f7265dbac100a12462c24f6
Marat Khamadeev <Marat.Khamadeev@gmail.com>
2013-07-05 12:39:33 +0400

    0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Promanap::Application.config.secret_token = 'a477e8210f19d5ca13daeaee9fb768fb4c00e9ae28033a6151cf88d826e4fd9d6b600f596b7225ae97e1497e352aad47087c5743a963e7c0ad089359744d6fd1'
